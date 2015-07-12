3724ff2044dcc6a6fe75847e59647416584453de
gr8linux <gr8linux@yahoo.com>
2015-07-12 23:58:27 +0430

    Add extra check on last update message file

--- frtbot.py
+++ frtbot.py
@@ -1,29 +1,30 @@
 import telegram as tg
 import time
-import os.path
+import os.path,os.stat
 #from subprocess import call,Popen
 import subprocess
 update_id_fname = "update_id.frt"
 update_id = -1
-if os.path.isfile(update_id_fname):
+if os.path.isfile(update_id_fname) and os.stat(update_id_fname).st_size !=0:
 	try:
 		update_id_fd = open(update_id_fname,"rb+")
+		update_id = int(update_id_fd.readline())
 	except IOError:
 		print "IOError accured!"
 		exit()
 else:
 	try:	
-		print "There is no "+update_id_fname+" file exit try to create newone!"
+		print "There is no "+update_id_fname+" file exit or it's empty try to create newone!"
 		update_id_fd = open(update_id_fname,"wb")
 		update_id = 0
 	except IOError:
 		print "IOError accured!"
 		exit()
-if update_id < 0 :
-	update_id = int(update_id_fd.readline())
-	print "The last update_id is :"+str(update_id)
-else:
-	update_id = 0
+#if update_id < 0 :
+#	update_id = int(update_id_fd.readline())
+#	print "The last update_id is :"+str(update_id)
+#else:
+#	update_id = 0
 	
 print "Strat the fortune Bot"
 bot = tg.Bot(token='<YOUR TOKEN>')
@@ -49,6 +50,8 @@ while(True):
 			update_id_fd.seek(0,0)
 			update_id_fd.write(str(update_id)+"\n")
 			#print update
+			if update.message.chat == None:
+				continue
 			bot.sendChatAction(update.message.chat_id,tg.ChatAction.TYPING)
 			fortune = subprocess.Popen("fortune", shell=True, stdout=subprocess.PIPE).stdout.read()
 			print update_id
